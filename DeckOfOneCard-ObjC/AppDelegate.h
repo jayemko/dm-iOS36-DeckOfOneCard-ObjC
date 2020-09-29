a56530c6103926011a6dc03f2965d0231a6fd0cd
Jason Koceja <jason@koceja.com>
2020-09-29 12:59:11 -0700

    initial commit

---
+++ DeckOfOneCard-ObjC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DeckOfOneCard-ObjC
+//
+//  Created by Jason Koceja on 9/29/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DeckOfOneCard-ObjC/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  DeckOfOneCard-ObjC
+//
+//  Created by Jason Koceja on 9/29/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
